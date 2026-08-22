c23a4514b6d06b83686d3fcb0f74eabc542ace66
SatvikPrabhu <satvikp.567@gmail.com>
2026-08-22 14:24:14 +0530

    Clear main branch

--- backend/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { predictImpact } = require("../controllers/aiController");
-
-const router = express.Router();
-
-router.post("/predict-impact", predictImpact);
-
-module.exports = router;
--- backend/routes/dataRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getUnifiedSpatial, getSummary } = require("../controllers/dataController");
-
-const router = express.Router();
-
-router.get("/unified-spatial", getUnifiedSpatial);
-router.get("/summary", getSummary);
-
-module.exports = router;
